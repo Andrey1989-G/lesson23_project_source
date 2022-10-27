a20382e3bdc8306217af200d0163a9176ef75861
Andrey1989-G <goreglyad.andrey@gmail.com>
2022-10-27 21:52:38 +0500

    commit

---
+++ utils.py
@@ -0,0 +1,24 @@
+def open_files(path: str):
+    """Открытие файла"""
+    try:
+        with open(path, encoding='utf-8') as f:
+            data_begin = f.readlines()
+            data_fin = [i.strip() for i in data_begin if len(i.strip()) > 0]
+        return data_fin
+    except FileNotFoundError:
+        print('файл не найден')
+
+def get_commands(command: str, value, data):
+    """Работаем с командами"""
+    if command == 'filter':
+        res = filter(lambda x: value in x, data)
+    elif command == 'map':
+        res = map(lambda x: x.split()[int(value)], data)
+    elif command == 'unique':
+        res = set(data)
+    elif command == 'sort':
+        res = sorted(data) if value == 'asc' else sorted(data, reverse=True)
+
+    return res
+
+# print(get_commands('sort', 'asc', open_files('data/apache_logs.txt')))
